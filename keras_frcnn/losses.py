37883f04265b3fd44d56e453cbd5d4ffceae8ffa
ngonhi@grinnell.edu <Nqk180998!>
2021-01-14 23:37:06 +0700

    fix version

--- keras_frcnn/losses.py
+++ keras_frcnn/losses.py
@@ -1,7 +1,7 @@
 from keras import backend as K
 from keras.objectives import categorical_crossentropy
 
-if K.common.image_data_format() == 'tf':
+if K.image_data_format() == 'tf':
 	import tensorflow as tf
 
 lambda_rpn_regr = 1.0
@@ -15,7 +15,7 @@ epsilon = 1e-4
 
 def rpn_loss_regr(num_anchors):
 	def rpn_loss_regr_fixed_num(y_true, y_pred):
-		if K.common.image_dim_ordering() == 'th':
+		if K.image_data_format() == 'th':
 			x = y_true[:, 4 * num_anchors:, :, :] - y_pred
 			x_abs = K.abs(x)
 			x_bool = K.less_equal(x_abs, 1.0)
