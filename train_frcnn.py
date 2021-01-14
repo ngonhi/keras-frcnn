a896fb451b21a1a1d4ee43297762bcb0afbb5b66
ngonhi@grinnell.edu <Nqk180998!>
2021-01-14 23:34:18 +0700

    fix version

--- train_frcnn.py
+++ train_frcnn.py
@@ -116,7 +116,7 @@ print(f'Num val samples {len(val_imgs)}')
 data_gen_train = data_generators.get_anchor_gt(train_imgs, classes_count, C, nn.get_img_output_length, K.common.image_dim_ordering(), mode='train')
 data_gen_val = data_generators.get_anchor_gt(val_imgs, classes_count, C, nn.get_img_output_length,K.common.image_dim_ordering(), mode='val')
 
-if K.common.image_dim_ordering() == 'th':
+if K.common.image_data_format() == 'th':
 	input_shape_img = (3, None, None)
 else:
 	input_shape_img = (None, None, 3)
